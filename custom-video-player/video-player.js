bd6697ab0a2950b5026f850c7340f893807a9ea2
komalapa <komalapa@gmail.com>
2021-08-27 23:21:52 +0400

    feat: slider for videos; doc: main console.log

--- custom-video-player/video-player.js
+++ custom-video-player/video-player.js
@@ -1,5 +1,56 @@
-const video = document.querySelector('#video-paris');
+console.log(`
+Прошу обратить внимание, что третье видео с фонтаном без звука. На нем хорошо видна скорость воспроизведения.
+Управление:
+        Shift + / / h / H - help (wait 10sec or click for exit)
+        k / Space         - play/pause
+        j / ←             - forward
+        l / →             - back
+        P / Shift + p     - previous video
+        N / Shift + n     - next video
+        ,                 - previous frame
+        .                 - next frame
+        <                 - slower
+        >                 - faster
+        f / F / Shift+f   - fullscreen
+        0..9              - navigation by tenths
+        ↑                 - volume up for all videos
+        ↓                 - volume down for all videos
+        m / M / Shift+m   - mute for all videos
 
+=======================================================
+1) Разобраться в коде проекта +10
+
+2) Управление плеером с клавиатуры: 
+    1) клавиша Пробел — пауза, 
+    2) Клавиша M (англ) — отключение/включение звука,
+    3) Клавиша > — ускорение воспроизведения ролика, 
+    4) Клавиша < — замедление воспроизведения ролика, 
+    5) Клавиша F — включение/выключение полноэкранного режим. 
+Горячие клавиши должны работать так же, как работают эти клавиши в YouTube видео
++10
+3) добавить поддержку других горячих клавиш из тех, которые поддерживаются в YouTube видео - 2 балла за каждую дополнительную горячую клавишу.
+Все, которым нашла применение. Вольность: для справки добавила h + стрелки клавиатуры управляют громкостью и прогрессом
+Shift + / / h / H - help (wait 10sec or click for exit)
+j / ←             - forward
+l / →             - back
+P / Shift + p     - previous video
+N / Shift + n     - next video
+,                 - previous frame
+.                 - next frame
+0..9              - navigation by tenths
+↑                 - volume up for all videos
+↓                 - volume down for all videos
+10шт +20
+
+добавить возможность перелистывания видео или слайдер видео Демо (в демо вместо картинок используйте видео)
+
+`)
+let video = document.querySelector('.video-active');
+const slider = document.querySelector('.video-slider');
+const slides = slider.querySelectorAll('video')
+const slidesLength = slides.length;
+const sliderDuration = 200;
+//const sliderWidth = slider.clientWidth;
 //controls
 const centralPlayBtn = document.querySelector('.video-player-start');
 const playBtn = document.querySelector('.video-player-controls-play');
@@ -21,8 +72,28 @@ volumeBar.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${vo
 
 const MSG_DURATION = 2000;
 
-const VIDEOS = ['assets/video/paris.mp4', "assets/video/TourEiffel.mp4", "assets/video/louvre.mp4"]
+//const VIDEOS = ['assets/video/paris.mp4', "assets/video/TourEiffel.mp4", "assets/video/louvre.mp4"]
 let activeVideo = 0;
+//sl -slider
+function slChangeSlide(direction){
+    const prevSlide = activeVideo;
+    video.pause();
+    if (direction === 'forward'){
+        activeVideo = activeVideo < slidesLength -1 ? activeVideo + 1 : 0
+    } else if (direction === 'back'){
+        activeVideo = activeVideo > 0 ? activeVideo - 1 : slidesLength -1    
+    }
+    slides[activeVideo].classList.add('video-active');
+    slides[prevSlide].classList.remove('video-active');
+    slides[prevSlide].style.left = 0;//timeout before previous slide will be moved
+    setTimeout (() => slides[prevSlide].style.left = null, sliderDuration)
+    video = slides[activeVideo];
+    //console.log(video.src)
+    video.play()   
+}
+
+
+//vp - video player
 function vpPlay(){
     video[video.paused ? 'play' : 'pause']();
     //msg
@@ -33,25 +104,26 @@ function vpPlay(){
     }, MSG_DURATION);
 }
 
-function vpChooseVideo(direction = 'back'){
-    const length = VIDEOS.length
-    if (direction === 'forward'){
-        activeVideo = activeVideo < length -1 ? activeVideo + 1 : 0
-    } else if (direction === 'back'){
-        activeVideo = activeVideo > 0 ? activeVideo - 1 : length -1    
-    }
-    video.src = VIDEOS[activeVideo]
-    video.play();
-    //msg
-    controlsMsg.innerText = `${direction}` 
-    controlsMsg.style.display = 'block';
-    setTimeout(() => {
-        controlsMsg.style.display = 'none';
-    }, MSG_DURATION);
-}
+// function vpChooseVideo(direction = 'back'){
+//     const length = VIDEOS.length
+//     if (direction === 'forward'){
+//         activeVideo = activeVideo < length -1 ? activeVideo + 1 : 0
+//     } else if (direction === 'back'){
+//         activeVideo = activeVideo > 0 ? activeVideo - 1 : length -1    
+//     }
+//     video.src = VIDEOS[activeVideo]
+//     video.play();
+//     //msg
+//     controlsMsg.innerText = `${direction}` 
+//     controlsMsg.style.display = 'block';
+//     setTimeout(() => {
+//         controlsMsg.style.display = 'none';
+//     }, MSG_DURATION);
+// }
 
 function vpMute(){
-    video.muted = !video.muted;
+    slides.forEach(video => {video.muted = !video.muted})
+    //video.muted = !video.muted;
     if (video.muted){
         muteBtn.classList.add("crossed")
     } else {
@@ -66,7 +138,8 @@ function vpMute(){
 }
 
 function vpSetVolume(e){
-    video.volume = e ? e.target.value/100 : volumeBar.value/100;
+    slides.forEach(video => video.volume = e ? e.target.value/100 : volumeBar.value/100)
+    if (video.muted) vpMute();
     volumeBar.style.background = `linear-gradient(to right, #24809E 0%, #24809E ${volumeBar.value}%, #c4c4c4 ${volumeBar.value}%, #c4c4c4 100%)`
     //msg
     controlsMsg.innerText = `Volume ${volumeBar.value}%` 
@@ -78,7 +151,7 @@ function vpSetVolume(e){
 function vpSetProgress(e){
     let value
     if (!e) {
-        console.log("not e")
+        //console.log("not e")
         value = progressBar.value;
     } else {
         value = e.target.value;
@@ -89,7 +162,8 @@ function vpSetProgress(e){
 function vpFullscreen(){
     if (!document.fullscreenElement) {
         document.querySelector('.video-player-wrp').requestFullscreen();
-        video.classList.add("fullscreen")
+        //video.classList.add("fullscreen")
+        slider.classList.add("fullscreen")
     } else if (document.exitFullscreen) {
         document.exitFullscreen();      
     } 
@@ -97,12 +171,13 @@ function vpFullscreen(){
 //fixes exit fullscreen by esc and button
 document.addEventListener('fullscreenchange', () => {
     if (!document.webkitIsFullScreen && !document.mozFullScreen && !document.msFullscreenElement) {
-        video.classList.remove("fullscreen") 
+        //video.classList.remove("fullscreen")
+        slider.classList.remove("fullscreen") 
     }
 })
 
 function vpReplaceIconPlay(){
-    console.log("icon")
+    ///console.log("icon")
     if (!video.paused){
         centralPlayBtn.style.opacity = 0;
         playBtn.classList.add('video-player-controls-pause');
@@ -115,12 +190,18 @@ function vpReplaceIconPlay(){
 //Listeners
 playBtn.addEventListener('click', vpPlay);
 centralPlayBtn.addEventListener('click', vpPlay);
-video.addEventListener('click', vpPlay);
-video.addEventListener('play',vpReplaceIconPlay);
-video.addEventListener('pause',vpReplaceIconPlay);
-video.addEventListener('ended',  () => vpChooseVideo('forward'))
-backBtn.addEventListener('click',() => vpChooseVideo('back'));
-forwardBtn.addEventListener('click', () => vpChooseVideo('forward'));
+slides.forEach(video =>{
+    video.addEventListener('click', vpPlay);
+    video.addEventListener('play',vpReplaceIconPlay);
+    video.addEventListener('pause',vpReplaceIconPlay);
+    video.addEventListener('ended',  () => slChangeSlide('forward'))
+})
+
+// video.addEventListener('ended',  () => vpChooseVideo('forward'))
+// backBtn.addEventListener('click',() => vpChooseVideo('back'));
+// forwardBtn.addEventListener('click', () => vpChooseVideo('forward'));
+backBtn.addEventListener('click',() => slChangeSlide('back'));
+forwardBtn.addEventListener('click', () => slChangeSlide('forward'));
 muteBtn.addEventListener('click', vpMute);
 fullscreenBtn.addEventListener('click', vpFullscreen);
 
@@ -140,8 +221,9 @@ function toggleKeys(e){
     } 
     const key = e.key;
     if (!key) return;
-    console.log(e.shiftKey,key)
+    //console.log(e.shiftKey,key)
     if (key === "k" || key === "л" || key === " ") {
+        e.preventDefault();
         vpPlay();
         return
     }
@@ -166,19 +248,21 @@ function toggleKeys(e){
         return
     }
     if (key === "P" || (e.shiftKey && key ==="p") || key === "З" || (e.shiftKey && key ==="з")) {
-        vpChooseVideo('back')
+        //vpChooseVideo('back')
+        slChangeSlide('back')
         return
     }
     if (key === "N" || (e.shiftKey && key ==="n") || key === "Т" || (e.shiftKey && key ==="т")) {
-        vpChooseVideo('forward')
+        //vpChooseVideo('forward')
+        slChangeSlide('forward')
         return
     }
 
-    if ((key === "," || key ==="б") && video.paused) {
+    if (!e.shiftKey && (key === "," || key ==="б") && video.paused) {
         video.currentTime -= 0.04//25frames per sec
         return
     }
-    if ((key === "." || key ==="ю") && video.paused) {
+    if (!e.shiftKey && (key === "." || key ==="ю") && video.paused) {
         video.currentTime += 0.04//25frames per sec
         return
     }
@@ -217,25 +301,28 @@ function toggleKeys(e){
         }, MSG_DURATION);
         return
     }
-    if (((e.shiftKey && key ==="/") || key === "?" )) {
+    if (((e.shiftKey && key ==="/") || (e.shiftKey && key ===",") || key === "?" || key === "H" || (e.shiftKey && key ==="h") || key === "Р" || e.shiftKey && key ==="р" || key ==="h" || key ==="р")) {
         //msg
         controlsMsg.innerText = `
-        k               - play
-        j / ←           - forward
-        l / →           - back
-        P / Shift + p   - previous video
-        N / Shift + n   - next video
-        ,               - previous frame
-        .               - next frame
-        <               - slower
-        >               - faster
-        f / F / Shift+f - fullscreen
-        0..9            - navigation by tenths
-        ↑               - volume up
-        ↓               - volume down
+        Shift + / / h / H - help (wait 10sec or click for exit)
+        k / Space         - play/pause
+        j / ←             - forward
+        l / →             - back
+        P / Shift + p     - previous video
+        N / Shift + n     - next video
+        ,                 - previous frame
+        .                 - next frame
+        <                 - slower
+        >                 - faster
+        f / F / Shift+f   - fullscreen
+        0..9              - navigation by tenths
+        ↑                 - volume up for all videos
+        ↓                 - volume down for all videos
+        m / M / Shift+m   - mute for all videos
         ` 
         controlsMsg.style.display = 'block';
         video.addEventListener('click', hideMsg);
+        setTimeout(hideMsg, MSG_DURATION * 5);
         return
     }
     if (e.key === "ArrowDown" && volumeBar.value >= 5) {
@@ -245,7 +332,7 @@ function toggleKeys(e){
     }
     if (e.key === "ArrowUp" && volumeBar.value <= 95) {
         volumeBar.value = +volumeBar.value + 5
-        console.log(volumeBar.value)
+        //console.log(volumeBar.value)
         vpSetVolume()
         return
     }
